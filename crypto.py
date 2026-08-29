3cbd29c0b842c4c988ea899b919917b003fbbe19
Ayush Singh <singhayush0919@gmail.com>
2026-08-29 17:07:07 +0530

    Changes to be committed: 	modified:   crypto.py

--- crypto.py
+++ crypto.py
@@ -8,38 +8,33 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.exceptions import InvalidSignature
 
 def demonstrate_hashing(text):
-        def hash_md5(val):
-            return hashlib.md5(val.encode()).hexdigest()
+    def hash_md5(val):
+        return hashlib.md5(val.encode()).hexdigest()
+    def hash_sha256(val):
+        return hashlib.sha256(val.encode()).hexdigest()
+    md5_result = hash_md5(text)
+    sha256_result = hash_sha256(text)
+    return md5_result, sha256_result
 
-        def hash_sha256(val):
-            return hashlib.sha256(val.encode()).hexdigest()
-
-        md5_result = hash_md5(text)
-        sha256_result = hash_sha256(text)
-        return md5_result, sha256_result
+def generate_key():
+    return Fernet.generate_key()
 
 def demonstrate_symmetric_encryption(text):
-    def generate_key():
-        return Fernet.generate_key()
-    
     def encrypt_message(key, message):
         return Fernet(key).encrypt(message.encode())
-    
     def decrypt_message(key, token):
         return Fernet(key).decrypt(token).decode()
-
     key = generate_key()
     encrypted = encrypt_message(key, text)
     decrypted = decrypt_message(key, encrypted)
     return key, encrypted, decrypted
 
 def generate_rsa_keypair():
-        private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
-        public_key = private_key.public_key()
-        return private_key, public_key
+    private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+    public_key = private_key.public_key()
+    return private_key, public_key
 
 def demonstrate_asymmetric_encryption(text):
-
     def rsa_encrypt(plain_text, public_key):
         plain_text = plain_text.encode()
         pad = padding.OAEP(
@@ -62,7 +57,6 @@ def demonstrate_asymmetric_encryption(text):
             )
         decrypted_bytes = private_key.decrypt(encrypted_bytes, pad)
         return decrypted_bytes.decode()
-
     priv_key, pub_key = generate_rsa_keypair()
     encrypted = rsa_encrypt(text, pub_key)
     decrypted = rsa_decrypt(encrypted, priv_key)
@@ -74,12 +68,10 @@ def demonstrate_password_hashing(password):
         salt = bcrypt.gensalt()
         hashed = bcrypt.hashpw(pswd, salt)
         return hashed
-
     def verify_password_bcrypt(password, hashed):
         encoded = password.encode('utf-8')
         hash = bcrypt.checkpw(encoded, hashed)
         return hash
-
     hash1 = hash_password_bcrypt(password)
     hash2 = hash_password_bcrypt(password)
     is_match_correct = verify_password_bcrypt(password, hash1)
@@ -92,12 +84,10 @@ def demonstrate_hmac(message):
         hmac_val = hmac.new(key, msg, hashlib.sha256)
         hmac_str = hmac_val.hexdigest()
         return hmac_str
-
     def verify_hmac(message, secret_key, hmac_str) -> bool:
         hmac_nval = hmac.new(secret_key.encode(), message.encode(), hashlib.sha256).hexdigest()
         cmpr = hmac.compare_digest(hmac_nval, hmac_str)
         return cmpr
-
     my_key = "my_secret_key"
     generated_hmac = generate_hmac(message, my_key)
     valid_check, invalid_check = verify_hmac(message, my_key, generated_hmac), verify_hmac(message, "wrong_key", generated_hmac)
@@ -115,7 +105,6 @@ def demonstrate_digital_signature(message: str):
             hashes.SHA256()
         )
         return signed
-
     def verify_signature(message: str, signed: bytes, public_key) -> bool:
         hashed = hashlib.sha256(message.encode()).digest()
         try:
@@ -131,7 +120,6 @@ def demonstrate_digital_signature(message: str):
             return True
         except InvalidSignature:
             return False
-
     priv_key, pub_key = generate_rsa_keypair()
     signature = sign_message(message, priv_key)
     valid_verify = verify_signature(message, signature, pub_key)
